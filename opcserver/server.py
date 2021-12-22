e0db468279c317c67dba05a1574240566e3d5abd
sagIoTPower <christof.strack@softwareag.com>
2021-12-22 14:45:35 +0100

    changed opc simulator

--- opcserver/server.py
+++ opcserver/server.py
@@ -6,6 +6,7 @@ import time
 import math
 import logging
 import datetime
+import random
 
 from opcua import ua, Server
 
@@ -25,18 +26,24 @@ if __name__ == "__main__":
 
     server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
     # setup our own namespace, not really necessary but should as spec
-    uri = "http://examples.freeopcua.github.io"
+    uri = "http://anlagenbauer.freeopcua.io"
     idx = server.register_namespace(uri)
 
     objects = server.get_objects_node()
 
     # get Objects node, this is where we should put our nodes
-    dev = server.nodes.base_object_type.add_object_type(idx, "MyDevice")
-    dev.add_variable(idx, "sensor1", 1.0).set_modelling_rule(True)
-    dev.add_property(idx, "device_id", "0340").set_modelling_rule(True)
+    types = server.get_node(ua.ObjectIds.BaseObjectType)
+    object_type_to_derive_from = server.get_root_node().get_child(["0:Types", 
+                                                                   "0:ObjectTypes", 
+                                                                   "0:BaseObjectType"])
+    # create object type & object
+    mycustomobj_type = types.add_object_type(idx, "Maschinen_Type_III")
+    mycustomobj_type.add_variable(0, "power_variable", 220.0).set_modelling_rule(True) #if false it would not be instantiated
+    mycustomobj_type.add_property(idx, "device_id", "123456").set_modelling_rule(True)
 
      # First a folder to organise our nodes
     myCompany = server.nodes.objects.add_folder(idx, "Maschinenbau")
+    myobj = myCompany.add_object(idx, "Maschinen Type III", mycustomobj_type.nodeid)
     myWerk = myCompany.add_folder(idx, "Werk 2")
     myCompany.add_folder(idx, "Werk 1")
     myCompany.add_folder(idx, "Werk 3")
@@ -46,17 +53,17 @@ if __name__ == "__main__":
     drives = myMachine.add_object(idx, "Drives")
     spindel = myMachine.add_object(idx, "Spindel")
     table = myMachine.add_object(idx, "Wechseltisch")
-    power=  drives.add_variable("ns=2;s=Drives/Power", "Power", 6.7)
+    power=  drives.add_variable(idx, "Power", 6.7)
     amplitude =  drives.add_variable(idx, "Amplitude", 20.0)
     current = drives.add_variable(idx, "Current", 6.7)
-    resolution = drives.add_variable(idx, "Resolution", 500)
+    resolution = drives.add_variable(idx, "Resolution", 100)
     power.set_writable()    # Set MyVariable to be writable by clients
     current.set_writable()
     resolution.set_writable()
     amplitude.set_writable()
     ctrl = myMachine.add_object(idx, "Controller")
     ctrl.set_modelling_rule(True)
-    state = ctrl.add_property(idx, "state", "Idle")
+    state = ctrl.add_property(idx, "State", "Idle")
     state.set_writable()
     state.set_modelling_rule(True)
 
@@ -67,34 +74,25 @@ if __name__ == "__main__":
     server.historize_node_data_change(power, period=None, count=100)
     try:
         i = 0
+        logger.info('Starting loop')
+        frequency = resolution.get_value()
+        state.set_value("active")
         while True:
             i = i + 1
-            time.sleep(1)
-            logger.info('Starting loop')
+            time.sleep(0.5)
             seconds = time.time()
-            frequency = 100
-            # logger.info('Frequency is ' + str(frequency))
-            # logger.info('Calculating power')
-            simPower = amplitude.get_value()*math.sin(seconds*frequency/100)
-            logger.info('Power is ' + str(simPower))
-            # logger.info('Changing values on server for resolution')
-            resolution.set_value(frequency)
-            # logger.info('Resolution is now: ' + str(frequency))
-            # logger.info('Changing values on server for power')
-            dv = ua.DataValue(2)
-            dv.ServerTimestamp = datetime.datetime.now()
-            power.set_value(dv)
-            power.set_value(round(simPower,1))
-            if (i <= 10):
-                state.set_value("active")
-            elif (i > 10 & i <= 20):
-                state.set_value("idle")
-            else:
+            simPower = amplitude.get_value() * abs( math.sin(seconds * frequency / 100) )
+            current.set_value( round(math.cos(seconds) * 10.0, 2))
+            frequency = frequency + 0.05 * frequency * random.randint(- 1,1 )   # Step direction (-1, 0, +1)
+            power.set_value(round(simPower,2))
+            if ( i == 50 ):
+                state.set_value("Idle")
+            elif ( i == 100 ):
+                state.set_value("Active")
+            elif ( i >= 150 ):
                 i = 0
             simState = state.get_value()
-            # logger.info('Power is now: ' + str(simPower))
-            logger.info('status is ' + str(simState))
-            logger.info('Loop ends, sleeping')
+            logger.info('Power: ' + str(round(simPower,2)) + ' Status: ' + str(simState))
     finally:
         #close connection, remove subcsriptions, etc
         server.stop()
